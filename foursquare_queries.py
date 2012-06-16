8a9ecab5a6e32a1ebb6f719912b6ab73f5b9f342
Onwukike Ibe <kikeibe@gmail.com>
2012-06-16 15:32:18 -0400

    added soccer too

--- foursquare_queries.py
+++ foursquare_queries.py
@@ -1,15 +1,19 @@
-#import pprint as pp
+import pprint as pp
 import json
 import requests
 
+
+fields_id =	'4bf58dd8d48988d15f941735'#Fields
+parks_id =  '4bf58dd8d48988d163941735'#parks
 meter_to_mile_convert =1609
 categories_dict= {}
 categories_dict['tennis'] = ['4e39a956bd410d7aed40cbc3',#Tennis Court
 							'4e39a9cebd410d7aed40cbc4']#College Tennis Court
 categories_dict['basketball'] = ['4bf58dd8d48988d1e1941735',#basketball court
 								 '4bf58dd8d48988d1ba941735']#college basketball
-categories_dict['frisbee'] = ['4bf58dd8d48988d15f941735',#Fields
-							  '4bf58dd8d48988d163941735']#parks
+categories_dict['frisbee'] = [fields_id,parks_id]
+categories_dict['soccer'] = [fields_id,parks_id]
+
 def grab_venues(sport,lat, lon):
 	url = 'https://api.foursquare.com/v2/venues/search'
 	queries=''
@@ -21,11 +25,12 @@ def grab_venues(sport,lat, lon):
 			'categoryId':queries,
 			'll':lat+","+lon,
 			'radius':radius_miles
-}
+	}
 	r = requests.get(url,params=payload)
 	#print r.url
 	#pp.pprint(json.loads(r.text))
 	return r.text
+
 """
 grab_venues(
 	'tennis',#sport
@@ -34,4 +39,4 @@ grab_venues(
 )
 """
 
-#grab_venues('frisbee','40.7','-74')
+#grab_venues('soccer','40.7','-74')
