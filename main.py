20d0ec49080f2eb502b9b777c48cf9f674040e9d
Sahat Yalkabov <sakhat@gmail.com>
2012-06-16 19:53:41 +0000

    2 post so far

--- main.py
+++ main.py
@@ -5,17 +5,67 @@ import json
 app = Flask(__name__)
 
 connection = Connection()
-db = connection.test_database
-post = {"author":"Mike","text":"Test"}
-
-posts = db.posts
-posts.insert(post)
-print 'done'
+db = connection.gameon
 
 @app.route("/api", methods=['POST', 'GET'])
 def api_response():
     if request.method == 'POST':
-        return request.json
+        return request.form['param_one'] + ' ' + request.form['param_two']
+    else:
+        return 'test'
+
+@app.route("/game/details")
+def game_details():
+    pass
+
+@app.route('/user/create', methods=['GET','POST'])
+def create_user():
+    if request.method == 'POST':
+        user = {
+            'fbid': request.form['id'],
+            'name': request.form['name'],
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'fblink': request.form['link'],
+            'gender': request.form['gender']
+        }
+        db.users.insert(user)
+        return 'Saved to MongoDB'
+    else:
+        return 'GET REQUEST, USE POST INSTEAD'
+
+
+@app.route("/game/create", methods=['POST'])
+def create_game():
+    if request.method == 'POST':
+        game = {
+            'sport': request.form['sport'],
+            'latitude': request.form['lat'],
+            'longitude': request.form['lon'],
+            'timestamp': request.form['timestamp'],
+            'number_of_players': request.form['numplayers'],
+            'creator': {
+                'name': request.form['name'],
+                'fbid': request.form['fbid']
+            }
+        }
+        db.games.insert(game)
+
+
+@app.route("/games/list")
+def list_games():
+    # sorted by closest long/lat coordinates, then by date/time
+    return db.gameon.games.find("loc": )
+
+@app.route("/game/join")
+def join_game():
+    pass
+
+@app.route("/<user>/games")
+def display_user_games():
+    pass
+
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
